fec1cdf52634399dab08eccf4b795679af3027a6
sivaram <sivaramsiva16@gmail.com>
2018-09-30 15:54:01 +0530

    back added

--- app/src/main/java/com/example/roshan/apartmentdemo/OwnerLoginActivity.java
+++ app/src/main/java/com/example/roshan/apartmentdemo/OwnerLoginActivity.java
@@ -4,6 +4,7 @@ import android.content.DialogInterface;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Toast;
 
@@ -13,7 +14,19 @@ public class OwnerLoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_owner_login);
+        getSupportActionBar().setTitle("Apartment Demo");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 
     public void performSignIn(View view) {
         Toast.makeText(this, "This action calls the sign-in method.", Toast.LENGTH_SHORT).show();
