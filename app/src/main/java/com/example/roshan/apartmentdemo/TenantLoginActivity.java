5261d14cf3c6ce3d907430aae5e9079f7f7cdc04
Roshan <roshanrahman6399@gmail.com>
2018-09-30 14:24:25 +0530

    Removed some code

--- app/src/main/java/com/example/roshan/apartmentdemo/TenantLoginActivity.java
+++ app/src/main/java/com/example/roshan/apartmentdemo/TenantLoginActivity.java
@@ -2,6 +2,8 @@ package com.example.roshan.apartmentdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class TenantLoginActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class TenantLoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tenant_login);
+    }
+
+    public void performSignIn(View view) {
+        Toast.makeText(this, "This action calls the sign-in method.", Toast.LENGTH_SHORT).show();
+    }
+
+    public void handleTroubleSigningIn(View view) {
+        Toast.makeText(this, "This action calls the trouble signing in method.", Toast.LENGTH_SHORT).show();
     }
 }
