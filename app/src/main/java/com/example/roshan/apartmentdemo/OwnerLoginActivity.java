f447ab879ac9f1940be349bbb0d3812dcd65ed8b
Roshan <roshanrahman6399@gmail.com>
2018-09-30 15:15:56 +0530

    Added alerts

--- app/src/main/java/com/example/roshan/apartmentdemo/OwnerLoginActivity.java
+++ app/src/main/java/com/example/roshan/apartmentdemo/OwnerLoginActivity.java
@@ -1,5 +1,7 @@
 package com.example.roshan.apartmentdemo;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -18,6 +20,38 @@ public class OwnerLoginActivity extends AppCompatActivity {
     }
 
     public void handleTroubleSigningIn(View view) {
-        Toast.makeText(this, "This action calls the trouble signing in method.", Toast.LENGTH_SHORT).show();
+        new AlertDialog.Builder(this).setMessage("If you have forgotten your password, you can generate a new one using an OTP.\n\nIf you have forgotten your user ID, please contact the administrator.")
+                .setTitle("Trouble signing in")
+                .setPositiveButton(
+                        "Forgot Password",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(OwnerLoginActivity.this, "Assume that OTP is sent to your registered mobile number", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                )
+                .setNegativeButton(
+                        "Forgot ID",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(OwnerLoginActivity.this, "Please contact your administrator for help.", Toast.LENGTH_LONG).show();
+                            }
+                        }
+                )
+                .setNeutralButton(
+                        "Cancel",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        }
+                ).setCancelable(false).create().show();
     }
+
+  
 }
+
+/* TODO: Need to implement back button in the action bar for this activity dude, so that it will go back to Main Activity. */
--- app/src/main/java/com/example/roshan/apartmentdemo/TenantLoginActivity.java
+++ app/src/main/java/com/example/roshan/apartmentdemo/TenantLoginActivity.java
@@ -1,5 +1,7 @@
 package com.example.roshan.apartmentdemo;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -18,7 +20,27 @@ public class TenantLoginActivity extends AppCompatActivity {
     }
 
     public void handleTroubleSigningIn(View view) {
-        Toast.makeText(this, "This action calls the trouble signing in method.", Toast.LENGTH_SHORT).show();
+        new AlertDialog.Builder(this).setMessage("Please contact the flat owner who has registered your tenant account for your login credentials.\n\nYou can request new ID from the owner if you want.")
+                .setTitle("Trouble signing in")
+                .setPositiveButton(
+                        "Request new ID",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(TenantLoginActivity.this, "Request sent to your flat owner", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                )
+                .setNegativeButton(
+                        "Cancel",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        }
+                ).setCancelable(false).create().show();
+
     }
 }
 
