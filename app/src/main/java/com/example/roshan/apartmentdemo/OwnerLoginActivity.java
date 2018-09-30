a9c6f56a17ca4d27a2fbae171eef8d937776d446
Roshan <roshanrahman6399@gmail.com>
2018-09-30 13:56:40 +0530

    Added new activities

---
+++ app/src/main/java/com/example/roshan/apartmentdemo/OwnerLoginActivity.java
@@ -0,0 +1,13 @@
+package com.example.roshan.apartmentdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class OwnerLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_owner_login);
+    }
+}
---
+++ app/src/main/java/com/example/roshan/apartmentdemo/TenantLoginActivity.java
@@ -0,0 +1,13 @@
+package com.example.roshan.apartmentdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TenantLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tenant_login);
+    }
+}
