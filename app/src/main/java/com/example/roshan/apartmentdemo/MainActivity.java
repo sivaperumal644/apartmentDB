4fb924e74cb06a3f03a9c1334e74dd1df9ebbac3
Roshan <roshanrahman6399@gmail.com>
2018-09-30 14:13:43 +0530

    Added intents

--- app/src/main/java/com/example/roshan/apartmentdemo/MainActivity.java
+++ app/src/main/java/com/example/roshan/apartmentdemo/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.roshan.apartmentdemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void startTenantLoginActivity(View view) {
+        startActivity(new Intent(this, TenantLoginActivity.class));
+    }
+
+    public void startOwnerLoginActivity(View view) {
+        startActivity(new Intent(this, OwnerLoginActivity.class));
     }
 }
